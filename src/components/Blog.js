28599b2cfbd502b782e265e3950c91ff69119e7d
David Puerta Martos <dpuerta@usal.es>
2021-12-01 20:22:19 +0100

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author.name}
-  </div>
-)
+import React, {useState} from 'react'
+
+const Blog = (props) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  
+  const [visible, setVisible] = useState(false)
+  
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return(
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {props.blog.title} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.blog.title} <button onClick={toggleVisibility}>hide</button>
+        <div>{props.blog.url}</div>
+        <div>Likes: {props.blog.likes} <button onClick="">Like</button></div>
+        <div>Posted by: {props.blog.author.name}</div>
+        
+      </div>
+    </div>
+  )
+} 
 
 export default Blog
